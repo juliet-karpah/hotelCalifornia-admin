5429afd6c1cc466c88fe7a6079b4594effb8c5d3
juliet-karpah <122952544+juliet-karpah@users.noreply.github.com>
2025-02-18 17:11:44 -0800

    completenext js tutorial

--- hotel-admin-dashboard/app/lib/actions.ts
+++ hotel-admin-dashboard/app/lib/actions.ts
@@ -3,20 +3,31 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({invalid_type_error: 'Please select a customer.'}),
+    amount: z.coerce.number().gt(0, { message: "Please enter an amount greater than $0" }),
+    status: z.enum(['pending', 'paid'], { invalid_type_error: 'Please select an invoice status.' }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createBookings(formData: FormData) {
+export async function createBookings(prevState: State, formData: FormData) {
     try {
         const rawFormData = {
             customerId: formData.get('customerId'),
@@ -24,7 +35,15 @@ export async function createBookings(formData: FormData) {
             status: formData.get('status'),
         };
 
-        const { customerId, amount, status } = CreateInvoice.parse(rawFormData)
+        const validatedFields = CreateInvoice.safeParse(rawFormData)
+        if (!validatedFields.success) {
+            return {
+                errors: validatedFields.error.flatten().fieldErrors,
+                message: 'Missing Fields. failed to Create Invoice.'
+            }
+        }
+
+        const { customerId, amount, status } = validatedFields.data
         const amountInCents = amount * 100;
         const date = new Date().toISOString().split('T')[0];
 
@@ -32,13 +51,13 @@ export async function createBookings(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
-
-        revalidatePath('/dashboard/invoices');
-        redirect('/dashboard/invoices');
     } catch (error) {
         console.error(error)
-        throw new Error('Error creating bookings')
+        return {message: "Error creating bookings"}
     }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 
 }
 
@@ -47,13 +66,21 @@ export async function createBookings(formData: FormData) {
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 // ...
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
     try {
-        const { customerId, amount, status } = UpdateInvoice.parse({
+        const validatedFields = UpdateInvoice.safeParse({
             customerId: formData.get('customerId'),
             amount: formData.get('amount'),
             status: formData.get('status'),
         });
+        if(!validatedFields.success){
+            return {
+                errors: validatedFields.error.flatten().fieldErrors,
+                message: "Missing Fields. Failed to Update Booking."
+            }
+        }
+
+        const {customerId, amount, status} = validatedFields.data
 
         const amountInCents = amount * 100;
 
@@ -63,21 +90,38 @@ export async function updateInvoice(id: string, formData: FormData) {
             WHERE id = ${id}
           `;
 
-        revalidatePath('/dashboard/invoices');
-        redirect('/dashboard/invoices');
     } catch (error) {
         console.error(error)
-        throw new Error("Error updating booking")
+        return {message: "Error updating booking"}
     }
 
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
-        revalidatePath('/dashboard/invoices');
     } catch (error) {
         console.error(error)
-        throw new Error("Error in deleting booking")
+        return {message: "Error in deleting booking"}
+    }
+
+    revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(prevState: String | undefined, formData: FormData){
+    try{
+        await signIn('credentials', formData);
+    }catch(error){
+        if (error instanceof AuthError){
+            switch(error.type){
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
     }
 }
--- readme.md
+++ readme.md
@@ -1,12 +1,21 @@
 # My Notes
 
 ## Hooks
+
 ### usePathname()
 This hook is used in client components to retrieve the current URL's pathname. 
 ```
 import { usePathname } from 'next/navigation'
 
 const pathname = usePathname()
+```
+
+### useSearchParams
+This hook is used to retrieve parameters from a URL in the client component. 
+```
+import { useSearchParams } from 'next/navigation'
+
+const search = searchParams.get(paramName)
 ```
 
 
@@ -28,15 +37,33 @@ const pathname = usePathname()
     - valuable for data shared to users or data that doesnt change often like product page or blogs.
 - Dynamite Rendering: The server sents the content whenever the user sents a request. This is appropriate when a page has real-time data, user specific content, and for data that can allow be accessed during request time like cookies and URL search parameters.
     - The app is as fast as the slowest data fetch.
+    - In nextjs calling a dynamic function in a route makes the entire route dynamite.
 - Streaming
     - Data transfer method that works by breaking a route into smaller chunks and progressively streams data from the server to the client when the data is available.
     - Streaming solves the issue of the webpage being blocked by slow data transfer.
     - It allows the user to view and interact with parts of the page without loading all the data.
     - Streaming can be added by using loading.tsx or suspense in the component.
     - A loading skeleton can be implimented in the loading.tsx file as a placeholder/fallback UI.
+    - A good practice is moving data fetches into components and then wrapping the components with suspense.
+- Partial Prendering: The technique of combining static, dynamic rendering, and streaming in one route.
+    - On page load, the static shell of the route loads.
+    - The shell has holes that the dynamic content will fill eg cart, dashboard info
+    - The dynamic content will be streamed in parallel.
+    - Enable partial prerendering by adding ppr in the next.config.mjs file
+    - export const experimental_ppr = true; should be added to a route to implement PPR
+
+## Routing
+- Route Groups
+    - It is used to organize files without affecting the URL path structure. To separate the application into sections with routing groups, you add parenthesis () to the folder name eg (marketing).
 
 
-## Error Handling 
+## Error Handling
+- The try/catch statement is used to gracefully handle errors in server actions. 
+- Error file can be used to create fallback UI when there is any error.
+    - The Error file is a client component.
+    - It accepts the error and reset props. The error props is an instance of javascript's error object. The reset prop is a function to reset the error boundary. 
+- Handing 404 errors with notFound function when the resource does not exist.
+
 
 
 ## Caching 
